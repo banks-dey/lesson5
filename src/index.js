125cc3c7aa62ead16b3fe6bb5752835fa30d99fb
banks-dey <espatb@gmail.com>
2024-12-09 10:15:37 +0000

    added src

---
+++ src/index.js
@@ -0,0 +1,19 @@
+//alert("Hello world");
+//alert("Patience"); 
+//prompt("what is your name");
+//prompt("How old are you");
+
+//let name =prompt("what is your name");
+//let age = prompt("How old are you?");
+//let place= prompt("where do you live");
+//alert(place);
+
+
+
+//Take a users name, age and location and alert the message below; Hi "name", you are "age" years old and you live at "place"
+let name= prompt("What is your name")
+let age= prompt("How old are you?")
+let mail= prompt("What is your email address")
+ 
+//alert("Hi " + name + " you are "+ age + " years old and you live " + place);
+alert(`hi ${name} you are ${age} years old and we will keep in touch via your email ${mail}`);
