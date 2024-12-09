3cac48e3f1a8237bc34bcb213c0da75e45d2400d
banks-dey <espatb@gmail.com>
2024-12-09 12:07:08 +0000

    updated src

--- src/index.js
+++ src/index.js
@@ -11,9 +11,22 @@
 
 
 //Take a users name, age and location and alert the message below; Hi "name", you are "age" years old and you live at "place"
-let name= prompt("What is your name")
-let age= prompt("How old are you?")
-let mail= prompt("What is your email address")
+//let name= prompt("What is your name")
+//let age= prompt("How old are you?")
+//let mail= prompt("What is your email address")
  
 //alert("Hi " + name + " you are "+ age + " years old and you live " + place);
-alert(`hi ${name} you are ${age} years old and we will keep in touch via your email ${mail}`);
+//alert(`hi ${name} you are ${age} years old and we will keep in touch via your email ${mail}`);
+
+function changeButtonStyle(){
+    button.style.backgroundColor ="teal";
+    button.style.color = "black";
+}
+
+let button = document.querySelector ("button");
+
+
+button.addEventListener("mouseleave", changeButtonStyle);
+
+
+
